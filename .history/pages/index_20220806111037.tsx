61725701d659cfb65bdbb982b7df84e5cc171742
mike <apolloproductionsdj@gmail.com>
2022-08-06 11:35:04 -0700

    checking to see if files commit

---
+++ .history/pages/index_20220806111037.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Mike's Blog</title>
+      
+      </Head>
+<h1>Welcome to my blog</h1>
+     
+    </div>
+  )
+}
+
+export default Home
